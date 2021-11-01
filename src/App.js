0fd54a581dd80b6306291a0abf4c7939adcee0fd
jannat56 <jannatnishat30@gmail.com>
2021-11-01 13:45:05 +0600

    all updated

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './Pages/Shared/Header/Header';
 import Home from './Pages/Home/Home';
-import About from './Pages/About/About';
 import Footer from './Pages/Footer/Footer';
 import AuthProvider from './contexts/AuthProvider';
 import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
 import Login from './Pages/Login/Login/Login';
 import AddService from './Pages/AddService/AddService';
 import Booking from './Pages/Booking/Booking/Booking';
-// import ManageServices from './Pages/ManageServices/ManageServices';
 import NotFound from './Pages/NotFound/NotFound';
 import MyOrders from './Pages/MyOrders/MyOrders';
 import ManageOrders from './Pages/ManageOrders/ManageOrders';
@@ -28,9 +26,6 @@ function App() {
 						<Route path='/home'>
 							<Home></Home>
 						</Route>
-						{/* <Route path='/about'>
-							<About></About>
-						</Route> */}
 						<PrivateRoute path='/booking/:serviceId'>
 							<Booking></Booking>
 						</PrivateRoute>
